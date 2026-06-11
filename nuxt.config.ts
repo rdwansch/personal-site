b14686ed46bdac43ebf4475955c253be55fafe94
rdwansch <ridwanrasyid345@gmail.com>
2026-06-11 13:46:04 +0700

    checkpoint: milestone 2.5 done

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content', '@nuxt/fonts'],
+  fonts: {
+    families: [
+      { name: 'Preahvihear', provider: 'google' },
+    ],
+  },
   css: ['~/assets/css/main.css'],
 })
